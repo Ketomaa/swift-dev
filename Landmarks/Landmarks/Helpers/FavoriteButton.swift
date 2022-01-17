b3518bd4c1d1bd21a3a35bd1c181ce8c51f537ee
Ketomaa <tketomaa5@gmail.com>
2022-01-17 10:43:54 +0200

    task 2
    done

---
+++ Landmarks/Landmarks/Helpers/FavoriteButton.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Tatu Ketomaa on 17.1.2022.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton()
+    }
+}
--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -6,6 +6,11 @@
 //
 
 import Foundation
+import Combine
+
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
 
 var landmarks: [Landmark] = load("landmarkData.json")
 
@@ -13,8 +18,8 @@ func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
 
     do {
