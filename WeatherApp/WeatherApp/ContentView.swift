7fe83994175ce634622f8d92e697f7a72909a484
Ketomaa <tketomaa5@gmail.com>
2022-01-24 13:58:09 +0200

    Weather app
    Done

--- WeatherApp/WeatherApp/ContentView.swift
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -1,9 +1,3 @@
-//
-//  ContentView.swift
-//  WeatherApp
-//
-//  Created by Tatu Ketomaa on 24.1.2022.
-//
 
 import SwiftUI
 
@@ -17,7 +11,7 @@ struct ContentView: View {
             TextField("Enter your city", text: $city, onCommit: {
                 self.weather.fetch(by: self.city)
             }).padding(.horizontal)
-            
+             
             GeometryReader{ gr in
                 CurrentWeather(weather: self.weather.current, height: self.selected == 0 ?
                                gr.size.height : gr.size.height * 0.50).animation(.easeInOut(duration: 1.5))
@@ -39,39 +33,3 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct MainWeatherView: View {
-    var temperature: Int
-    var weather: Weather?
-    var body: some View {
-        VStack(spacing: 8) {
-            Image(uiImage: "https://openweathermap.org/img/wn/\(weather?.weather[0].icon ?? "")@2x.png".load())
-                .renderingMode(.original)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 180, height: 180, alignment: .center)
-            Text(weather?.weather[0].description ?? "")
-                .font(.system(size: 30, weight: .medium))
-                .foregroundColor(.white)
-            Text("\(String(format: "%.1f", weather?.main.temp ?? 0)) °C")
-                .font(.system(size: 70, weight: .medium))
-                .foregroundColor(.white)
-        }
-        .padding(.bottom, 40)
-    }
-}
-
-extension String {
-    func load() -> UIImage {
-        do {
-            guard let url = URL(string: self) else {
-                return UIImage()
-            }
-            let data: Data = try
-            Data(contentsOf: url)
-            return UIImage(data: data) ?? UIImage()
-        } catch {
-            
-        }
-        return UIImage()
-    }
-}
--- WeatherApp/WeatherApp/CurrentWeather.swift
+++ WeatherApp/WeatherApp/CurrentWeather.swift
@@ -15,7 +15,13 @@ struct CurrentWeather: View{
                 .shadow(color: .gray, radius: 12)
                 .foregroundColor(Color.white)
                 Spacer()
-                
+            
+                Image(uiImage: "https://openweathermap.org/img/wn/\(weather?.weather[0].icon ?? "")@2x.png".load())
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 180, alignment: .center)
+            
                 Text("\(weather?.main.temp.round ?? 0) °C")
                     .foregroundColor(.white)
                     .fontWeight(Font.Weight.heavy)
@@ -38,6 +44,24 @@ struct CurrentWeather: View{
             .edgesIgnoringSafeArea([.top, .bottom])
     }
 }
+
+extension String {
+    func load() -> UIImage {
+        do {
+            
+            guard let url = URL(string: self) else {
+                return UIImage()
+            }
+            let data: Data = try
+            Data(contentsOf: url)
+            return UIImage(data: data) ?? UIImage()
+        } catch {
+            
+        }
+        return UIImage()
+    }
+}
+
 struct CurrentWeather_Previews: PreviewProvider {
     static var previews: some View {
         CurrentWeather()
