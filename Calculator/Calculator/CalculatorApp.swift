1985d56fa88e6ecf80043d1564d7f10fd966ff22
Ketomaa <tketomaa5@gmail.com>
2022-02-07 14:17:37 +0200

    Calculator
    Calculator finaly

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tatu Ketomaa on 7.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
